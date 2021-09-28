279f76c5f76bfa5722d7b7be7942ae66abe78482
Mitchel <34202@ma-web.nl>
2021-09-28 11:53:46 +0200

    Operators klaar

---
+++ Operators.py
@@ -0,0 +1,16 @@
+name = "erwin henraat"
+job = "teacher"
+moneyInAccount = 1300
+
+def buyABrandNewMotorcycle():
+    for index in range(100):
+        print(":)")
+
+if name == "erwin henraat" and job != None or moneyInAccount > 10000:
+    buyABrandNewMotorcycle()
+
+age = 17
+name = "Mitchel"
+
+if age == 17 or age == 18 and name == "Mitchel":
+    print(f"Hallo {name}.")
