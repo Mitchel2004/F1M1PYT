3144cad90f95ee9da583d834ebd6e87e2bc4c510
Mitchel Klijn <90683368+Mitchel2004@users.noreply.github.com>
2021-09-28 10:28:04 +0200

    Add files via upload

---
+++ Slicing.py
@@ -0,0 +1,23 @@
+tekst = "De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde"
+print(tekst[37:41])
+print(tekst[43:48])
+print(tekst[61:])
+
+print("")
+
+tekst = "SD vakken zijn bijvoorbeeld Python, UXD, Frontend development en Backend development. Wat leuk!"
+print(tekst[28:34])
+print(tekst[36:39])
+print(tekst[41:61])
+print(tekst[65:84])
+
+print("")
+
+tekst = "Wat is hier het laatste woord?"
+print(tekst[-6:])
+
+print("")
+
+tekst = "Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee"
+print(tekst[5:9])
+print(tekst[29:33])
