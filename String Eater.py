f05a7a408114f9befbc722eb53b0b370bc13adac
Mitchel <34202@ma-web.nl>
2021-10-12 10:48:47 +0200

    String Eater klaar

--- String Eater.py
+++ String Eater.py
@@ -1,10 +1,24 @@
 string = "Hallo, ik ben een string en ik word opgegeten."
 
-def eat():
+class the_length:
     length = len(string)
+
+def eat():
+    if the_length.length > 0:
+        def eating():
+            print(string[:the_length.length])
+            the_length.length -= 1
+            eat()
+        
+        eating()
+    else:
+        exit()
+
+# def eat():
+    # length = len(string)
     
-    while length > 0:
-        print(string[:length])
-        length -= 1
+    # while length > 0:
+        # print(string[:length])
+        # length -= 1
 
 eat()
